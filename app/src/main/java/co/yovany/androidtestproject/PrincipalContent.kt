08146847049eb7c7de9fa0956f2fa515eac0c000
EDWIN ORBES <edwin20genius@gmail.com>
2018-04-26 15:46:57 -0500

    Toolbar with Searcher and TabLayout (Principal Menu)

---
+++ app/src/main/java/co/yovany/androidtestproject/PrincipalContent.kt
@@ -0,0 +1,12 @@
+package co.yovany.androidtestproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalContent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal_content)
+    }
+}
