56ce61575171daaabeac74e32ec30be6aca3c60a
EDWIN ORBES <yovany.dev@gmail.com>
2018-08-04 12:50:02 -0500

    Se modificó Tema de TextInputLayout y el componente Hint

---
+++ app/src/main/java/co/yovany/androidtestproject/view/ComponentEditTextActivity.java
@@ -0,0 +1,15 @@
+package co.yovany.androidtestproject.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.yovany.androidtestproject.R;
+
+public class ComponentEditTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component_edit_text);
+    }
+}
