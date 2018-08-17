14cfb005c8154aa81cc883fb3cd5610eff4362f1
EDWIN ORBES <yovany.dev@gmail.com>
2018-08-17 11:11:27 -0500

    Analizando el resaltado de valores en el gráfico

--- app/src/main/java/co/yovany/androidtestproject/view/MPAndroidChartActivity.java
+++ app/src/main/java/co/yovany/androidtestproject/view/MPAndroidChartActivity.java
@@ -1,6 +1,5 @@
 package co.yovany.androidtestproject.view;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +9,11 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.ChartHighlighter;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,8 @@ import java.util.Vector;
 import co.yovany.androidtestproject.R;
 import co.yovany.androidtestproject.model.Notes;
 
-public class MPAndroidChartActivity extends AppCompatActivity implements OnChartGestureListener {
+public class MPAndroidChartActivity extends AppCompatActivity implements OnChartGestureListener,
+        OnChartValueSelectedListener{
 
     LineChart lineChart;
 
@@ -33,21 +36,21 @@ public class MPAndroidChartActivity extends AppCompatActivity implements OnChart
                 .showToolbar(getResources().getString(R.string.mpandroidchart_activity_title), false);
 
         lineChart = findViewById(R.id.lineChart);
-        Vector<String> values = Notes.buildGraphicValues(1);
-        List<Entry> entries = new ArrayList<>();
+        List<Entry> entriesStudent1 = buildEntries(1);
 
-        for (String value : values) {
-            String[] elements = value.split("-");
-            entries.add(new Entry(
-                    Float.parseFloat(elements[1]),
-                    Float.parseFloat(elements[0])));
-        }
+        LineDataSet dataSet = new LineDataSet(entriesStudent1,getResources().getString(R.string.linechart_legend));
 
-        LineDataSet dataSet = new LineDataSet(entries,getResources().getString(R.string.linechart_legend));
-        dataSet.setColor(R.color.colorPositiveSelection);
-        dataSet.setValueTextColor(R.color.colorNegativeSelection);
-        dataSet.setFillColor(R.color.colorPositiveSelection);
+        //Controla el color de la linea del gráfico
+        dataSet.setColor(getResources().getColor(R.color.colorPositiveSelection));
+        //Controla el color de los valores de cada punto en el gráfico
+        dataSet.setValueTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        //Controla el color
+        dataSet.setFillColor(getResources().getColor(R.color.colorPositiveSelection));
+        //Controla el color de los diferentes puntos en el gráfico
         dataSet.setCircleColor(getResources().getColor(R.color.colorAccent));
+
+        //Destacando valores
+        highlightingValues(dataSet);
 
         LineData lineData = new LineData(dataSet);
         lineChart.setData(lineData);
@@ -57,11 +60,32 @@ public class MPAndroidChartActivity extends AppCompatActivity implements OnChart
         loadPropertiesLineChart();
 
         lineChart.setOnChartGestureListener(this);
+        lineChart.setOnChartValueSelectedListener(this);
     }
 
     /*==============================================================================================
     * FUNCIONES
      */
+
+    /*----------------------------------------------------------------------------------------------
+    * Contruye una Lista de entradas dependiendo del estudiante seleccionado
+    *
+    * @param student : Codigo del estudiante
+    *
+    * @return <code>List<Entry></code> : lista de entradas*/
+    private List<Entry> buildEntries(int student) {
+        Vector<String> values = Notes.buildGraphicValues(student);
+        List<Entry> listEntries = new ArrayList<>();
+
+        for (String value : values) {
+            String[] elements = value.split("-");
+            listEntries.add(new Entry(
+                    Float.parseFloat(elements[1]),
+                    Float.parseFloat(elements[0])));
+        }
+
+        return listEntries;
+    }
 
     /*----------------------------------------------------------------------------------------------
     * Evalúa las propiedades de la clase LineChart
@@ -100,6 +124,61 @@ public class MPAndroidChartActivity extends AppCompatActivity implements OnChart
          * Valores altos: Mayor lapso de desaceleración
          * Valores bajos: Menor lapso de desaceleración*/
         lineChart.setDragDecelerationFrictionCoef(0.9f);
+
+
+        lineChart.setHighlightPerDragEnabled(true);
+
+        //Deshabilita/Habilita el resaltado al seleccionar una valor del gráfico
+        lineChart.setHighlightPerTapEnabled(true);
+
+        /*Controla la distancia maxima del punto/valor al evento [tap] del usuario sobre el gráfico
+        * Default: 500dp*/
+        lineChart.setMaxHighlightDistance(200);
+
+        /*Establece un valor a resaltar en el gráfico
+        * @param x
+        *   Valor de un punto en el gráfico
+        *   (value1 = 2) AND (value2 = 3)
+        *   [2.0 >= x <= 2.4] -> valor1 resaltado
+        *   [2.5 >= x <= 3] -> valor2 resaltado
+        * @param dataSetIndex
+        * @param callListener */
+
+        lineChart.highlightValue(2.6f, 0, true);
+
+        //Highlight highlight = new Highlight(3.0f, 0);
+        /*Establece un valor a resaltar en el gráfico
+         * @param highlight Objeto para Resaltar
+         * @param callListener*/
+        //lineChart.highlightValue(highlight,false);
+
+        //Highlight[] highlights = new Highlight[2];
+        //highlights[0] = new Highlight(1.0f, 0);
+        //highlights[1] = new Highlight(2.0f, 0);
+        /*Establece una serie de valores a resaltar
+        * @param Highlight[] Vector de objetos tipo Highlight*/
+        //lineChart.highlightValues(highlights);
+
+        //Obtiene los elementos resaltados hasta el momento por el usuario
+        Highlight[] highlights = lineChart.getHighlighted();
+    }
+
+    /*----------------------------------------------------------------------------------------------
+    Resaltado de valores en el gráfico
+     */
+    private void highlightingValues(LineDataSet lineDataSet) {
+        //Deshabilita/Habilita el resaltado de valores en el gráfico
+        lineDataSet.setHighlightEnabled(true);
+        //Deshabilita/Habilita el resaltado (lineas)
+        lineDataSet.setDrawHighlightIndicators(true);
+        //Controla el color del resaltado
+        lineDataSet.setHighLightColor(getResources().getColor(R.color.colorNegativeSelection));
+        //Deshabilita/Habilita el resaltado de la linea vertical
+        lineDataSet.setDrawVerticalHighlightIndicator(true);
+        //Deshabilita/Habilita el resaltado de la linea horizontal
+        lineDataSet.setDrawHorizontalHighlightIndicator(true);
+        //Controla el ancho de la linea resaltada (dp)
+        lineDataSet.setHighlightLineWidth(2);
     }
 
     //Evento llamado cuando un [touch-gesture] ha empezado sobre el gráfico
@@ -149,5 +228,15 @@ public class MPAndroidChartActivity extends AppCompatActivity implements OnChart
     @Override
     public void onChartTranslate(MotionEvent me, float dX, float dY) {
         //Log.e("LINECHART-EVENT","TRANSLATE");
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.e("HIGHLIGHT",""+h.toString());
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }
